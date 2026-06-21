c388625bb47a2fdbc71812d7db29cfd731dbc001
Svyatoslav Pechurkin <pechurkins@gmail.com>
2026-06-21 21:59:26 +0300

    additional localization

--- src/main/java/me/proj/configs/DataSeeder.java
+++ src/main/java/me/proj/configs/DataSeeder.java
@@ -3,35 +3,39 @@ package me.proj.configs;
 import jakarta.transaction.Transactional;
 import me.proj.entities.Project;
 import me.proj.entities.User;
+import me.proj.repos.AvailabilityRepository;
 import me.proj.repos.ProjectRepository;
 import me.proj.repos.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
-public class DataSeeder {
+public class DataSeeder implements CommandLineRunner {
     private final ProjectRepository projectRepository;
     private final UserRepository userRepository;
     private final JdbcTemplate jdbcTemplate;
+    private final MessageSource messageSource;
 
-    public DataSeeder(ProjectRepository projectRepository, UserRepository userRepository, JdbcTemplate jdbcTemplate) {
+    public DataSeeder(ProjectRepository projectRepository, UserRepository userRepository, JdbcTemplate jdbcTemplate, MessageSource messageSource) {
         this.projectRepository = projectRepository;
         this.userRepository = userRepository;
         this.jdbcTemplate = jdbcTemplate;
+        this.messageSource = messageSource;
     }
 
-    @Transactional
-    public void seed() {
+    @Override
+    public void run(String... args) throws Exception {
+        String basicEvent = messageSource.getMessage("basicEvent", null, LocaleContextHolder.getLocale());
+
         Project defaultProject = projectRepository.findAll()
                 .stream()
                 .findFirst()
-                .orElseGet(() -> projectRepository.save(new Project("Общее мероприятие")));
-
-        if (userRepository.count() == 0) {
-            User user = userRepository.save(new User("Свят", "#3b82f6"));
-            defaultProject = projectRepository.findById(defaultProject.getId()).orElseThrow();
-            defaultProject.getUsers().add(user);
-        }
+                .orElseGet(() -> projectRepository.save(new Project(basicEvent)));
 
         migrateProjectMemberships(defaultProject);
     }
--- src/main/java/me/proj/configs/LocalizationConfig.java
+++ src/main/java/me/proj/configs/LocalizationConfig.java
@@ -1,7 +1,9 @@
 package me.proj.configs;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,6 +26,14 @@ public class LocalizationConfig implements WebMvcConfigurer {
         LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
         interceptor.setParamName("lang");
         return interceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 
     @Override
